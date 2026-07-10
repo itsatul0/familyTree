d0ba79e7eedd5b359b366766f9c5ddd3a4b1433c
Atul Sinha <itsatul0@gmail.com>
2026-07-10 17:03:29 +0530

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,9 +1,9 @@
 window.FAMILY_TREE_FIREBASE_CONFIG = {
-  apiKey: "REPLACE_ME",
+  apiKey: "AIzaSyAIzQeAhFjoylQqsaMQCWzHSr5T4hej6yg",
   authDomain: "my-family-tree-e28a9.firebaseapp.com",
   databaseURL: "https://my-family-tree-e28a9-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "my-family-tree-e28a9",
   storageBucket: "my-family-tree-e28a9.firebasestorage.app",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  messagingSenderId: "394004312057",
+  appId: "1:394004312057:web:24abe5380f122f6a115728"
 };
